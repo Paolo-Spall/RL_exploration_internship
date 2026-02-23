fa362812b88d903f861b4af0d0f5b1bc1cf17801
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2026-02-24 00:24:06 +0100

    only script_trai.py

--- script_train.py
+++ script_train.py
@@ -15,12 +15,26 @@ def play_sound(file_path):
 if len(sys.argv) > 1:
     model_names = sys.argv[1:]
 else:
-    model_names = ['MultiObsFrontierEnv_absolute_DQN_1e5',
- 'MultiObsFrontierEnv_absolute_agent_DQN_1e5',
- 'MultiObsFrontierEnv_absolute_iGain_DQN_1e5',
- 'MultiObsFrontierEnv_absolute_agent_iGain_DQN_1e5',
- 'MultiObsFrontierEnv_relative_DQN_1e5',
- 'MultiObsFrontierEnv_relative_agent_DQN_1e5',]
+    model_names = [
+ #'MultiObsFrontAvoidanceEnv_absolute_DQN_1e5',
+ 'MultiObsFrontAvoidanceEnv_absolute_agent_DQN_1e5',
+ #'MultiObsFrontAvoidanceEnv_absolute_iGain_DQN_1e5',
+ 'MultiObsFrontAvoidanceEnv_absolute_agent_iGain_DQN_1e5',
+ 'MultiObsFrontAvoidanceEnv_relative_DQN_1e5',
+ #'MultiObsFrontAvoidanceEnv_relative_agent_DQN_1e5',
+ 'MultiObsFrontAvoidanceEnv_relative_iGain_DQN_1e5',
+ #'MultiObsFrontAvoidanceEnv_relative_agent_iGain_DQN_1e5',
+ 'MultiObsFrontAvoidanceEnv_distance_DQN_1e5',
+ #'MultiObsFrontAvoidanceEnv_distance_agent_DQN_1e5',
+ 'MultiObsFrontAvoidanceEnv_distance_iGain_DQN_1e5',]
+ #'MultiObsFrontAvoidanceEnv_distance_agent_iGain_DQN_1e5']
+
+#     model_names = ['MultiObsFrontierEnv_absolute_DQN_1e5',
+#  'MultiObsFrontierEnv_absolute_agent_DQN_1e5',
+#  'MultiObsFrontierEnv_absolute_iGain_DQN_1e5',
+#  'MultiObsFrontierEnv_absolute_agent_iGain_DQN_1e5',
+#  'MultiObsFrontierEnv_relative_DQN_1e5',
+#  'MultiObsFrontierEnv_relative_agent_DQN_1e5',]
 #  'MultiObsFrontierEnv_relative_iGain_DQN_1e5',
 #  'MultiObsFrontierEnv_relative_agent_iGain_DQN_1e5',
 #  'MultiObsFrontierEnv_distance_DQN_1e5',
@@ -28,19 +42,6 @@ else:
 # 'MultiObsFrontierEnv_distance_iGain_DQN_1e5',
 #  'MultiObsFrontierEnv_distance_agent_iGain_DQN_1e5']
  
-#     model_names = [
-#  'MultiObsFrontAvoidanceEnv_absolute_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_absolute_agent_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_absolute_iGain_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_absolute_agent_iGain_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_relative_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_relative_agent_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_relative_iGain_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_relative_agent_iGain_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_distance_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_distance_agent_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_distance_iGain_DQN_1e5',
-#  'MultiObsFrontAvoidanceEnv_distance_agent_iGain_DQN_1e5']
 
 table_file = "models/evaluation_results_0.txt"
 n=0
