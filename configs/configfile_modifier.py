ca57f71fc524aad5b006bce292941fa88d332661
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2026-03-17 13:09:38 +0100

    config files

--- configs/configfile_modifier.py
+++ configs/configfile_modifier.py
@@ -13,11 +13,12 @@ for file in file_list:
         #config['model']['exploration_fraction'] = 0.7
 
         
-        index = file.find('frac07')
-        new_file_name = file[:index] + '_' + file[index:]
+        # index = file.find('frac07')
+        # new_file_name = file[:index] + '_' + file[index:]
         # index = file.find('1e5')
         # new_file_name = file[:index] + '5e5' + file[index+3:]
         #new_file_name = file.strip('.yaml') + '_frac07' + '.yaml'
+        new_file_name = file.strip('yaml')  + '.yaml'
         os.rename(file, new_file_name)
         # write the updated config back to the file
         with open(new_file_name, 'w') as f:
