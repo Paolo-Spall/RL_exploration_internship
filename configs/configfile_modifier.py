6cd55345f87525f1635922787e2c9fceb090bca7
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2026-03-17 01:08:50 +0100

    config files

--- configs/configfile_modifier.py
+++ configs/configfile_modifier.py
@@ -9,12 +9,15 @@ for file in file_list:
         with open(file, 'r') as f:
             config = yaml.safe_load(f)
         
-        config['training']['total_timesteps'] = 500000
-        # config['model']['exploration_fraction'] = 0.2
+        #config['training']['total_timesteps'] = 500000
+        #config['model']['exploration_fraction'] = 0.7
 
         
-        index = file.find('1e5')
-        new_file_name = file[:index] + '5e5' + file[index+4:]
+        index = file.find('frac07')
+        new_file_name = file[:index] + '_' + file[index:]
+        # index = file.find('1e5')
+        # new_file_name = file[:index] + '5e5' + file[index+3:]
+        #new_file_name = file.strip('.yaml') + '_frac07' + '.yaml'
         os.rename(file, new_file_name)
         # write the updated config back to the file
         with open(new_file_name, 'w') as f:
