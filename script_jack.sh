42c2ce0ad625fcbf7955989c738a65968dc2b088
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2026-03-19 18:42:44 +0100

    script files

--- script_jack.sh
+++ script_jack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-python3 script_train.py -dir batch_18-03_avoidance_5e5_frac07_reverse
-python3 script_train.py -dir batch_18-03_avoidance_frac07_2e5_reverse
-python3 script_train.py -dir batch_18-03_avoidance_frac02_2e5_reverse
 python3 script_train.py -dir batch_18-03_avoidance_frac07_reverse
 python3 script_train.py -dir batch_18-03_avoidance_frac02_reverse
+python3 script_train.py -dir batch_18-03_avoidance_frac07_2e5_reverse
+python3 script_train.py -dir batch_18-03_avoidance_frac02_2e5_reverse
+python3 script_train.py -dir batch_18-03_avoidance_5e5_frac07_reverse
