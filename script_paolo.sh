594dde0ced00cac314513cb1e130bf5ee301d038
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2026-03-21 00:20:34 +0100

    script files

--- script_paolo.sh
+++ script_paolo.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python3 script_train.py -dir batch_18-03_avoidance_5e5_frac02_reverse
+python3 script_train.py -dir batch_20-03_wrappers-static_nowrap-dynamic_anti-tilt_no-prog-stop_5e5_frac02_reverse
