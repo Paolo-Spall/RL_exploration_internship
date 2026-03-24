f143a22b0a15c341bde5463c71c376beaccb6863
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2026-03-24 01:16:36 +0100

    config files

--- configs/batch_23-03_learningr_rate_tries/configfile_modifier.py
+++ configs/batch_23-03_learningr_rate_tries/configfile_modifier.py
@@ -11,18 +11,19 @@ for file in file_list:
         with open(file, 'r') as f:
             config = yaml.safe_load(f)
         
-        #config['training']['total_timesteps'] = 1000000
-        config['model']['exploration_fraction'] = 0.5   
-
-        mod_name = config['model_name']
+        config['training']['total_timesteps'] = 500000
+        #config['model']['exploration_fraction'] = 0.5   
+        #new_model_name = file.strip('config_').strip('.yaml') + '_frac07'
+        #config['model_name'] = new_model_name
+        #mod_name = config['model_name']
         # # index = mod_name.find('1e5')
         # # new_file_name = file[:index] + 'frac02_' + '2e5' + file[index+3:]
         # new_mod_name = mod_name.replace('_frac07', '')
         
         #new_mod_name = mod_name.replace('5e5', '1e6')
-        new_mod_name = mod_name.replace('frac02', 'frac05')
+        #new_mod_name = mod_name.replace('frac02', 'frac05')
         # new_mod_name = mod_name + '_frac07'
-        config['model_name'] = new_mod_name
+        #config['model_name'] = new_mod_name
 
         # index = file.find('frac07')
         # new_file_name = file[:index] + '_' + file[index:]
@@ -32,10 +33,10 @@ for file in file_list:
         # new_file_name = file.strip('yaml')  + '.yaml'
         
         #new_file_name = file.replace('5e5', '1e6')
-        new_file_name = file.replace('frac02', 'frac05')
+        #new_file_name = file.replace('frac02', 'frac05')
         # new_file_name = "config_" + file + "_frac07.yaml"
-        os.rename(file, new_file_name)
-        #new_file_name = file
+        #os.rename(file, new_file_name)
+        new_file_name = file
 
         # write the updated config back to the file
         with open(new_file_name, 'w') as f:
