4de8c211b899f3beeca41f810c01b5a7f2b88ef9
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2026-03-26 18:26:24 +0100

    configs

--- script_jack.sh
+++ script_jack.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python3 script_train.py -dir batch_26-03_distance_sorted_1e5
+python3 script_train.py -dir batch_25-03_abs-agent_30x30_perc5_prob022_5e5
--- script_vale_2.sh
+++ script_vale_2.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python3 script_train.py -dir batch_25-03_abs-agent-igain_30x40_perc5_prob022_1e6
+python3 script_train.py -dir batch_25-03_abs-agent_30x30_perc5_prob022_5e5
