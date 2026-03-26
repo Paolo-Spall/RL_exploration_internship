3037079c2dab385a2d80b688e2149f328e4f296b
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2026-03-26 08:30:56 +0100

    configs

--- script_vale_2.sh
+++ script_vale_2.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
-python3 script_train.py -dir batch_25-03_abs-agent_not-sorted_padding-1_lr5e-4_1e6
+python3 script_train.py -dir batch_25-03_abs-agent-igain_not-sorted_padding-1_dynamic_1e6
+python3 script_train.py -dir batch_25-03_abs-agent-igain_not-sorted_padding-1_30x40_1e6
