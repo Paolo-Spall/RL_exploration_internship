dbf7b25219942ecf44ae671261122279c583c43c
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2026-03-31 23:14:47 +0200

    config files

--- script_jack.sh
+++ script_jack.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
-python3 script_train.py -dir batch_31-03_abs-agent_not-sorted_padding-1_2e6
+python3 script_train.py -dir batch_31-03_abs-agent_7centroids_1e6
+python3 script_train.py -dir batch_31-03_abs-agent-igain_7centroids_1e6
